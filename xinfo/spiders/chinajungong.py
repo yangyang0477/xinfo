15488ce3d9eed6af8c0259ef90fc4a8bfef66ec2
yang.yang <0477>
2019-06-23 22:08:55 +0800

    test

--- xinfo/spiders/chinajungong.py
+++ xinfo/spiders/chinajungong.py
@@ -9,7 +9,7 @@ class ChinajungongSpider(CrawlSpider):
     name = 'chinajungong'
     allowed_domains = ['chinajungong.com']
     start_urls = ['http://www.chinajungong.com/News/']
-    #http://www.chinajungong.com/News/201906/36518.html
+    http://www.chinajungong.com/News/201906/36518.html
     rules = (
         Rule(LinkExtractor(allow=r'News\/\d+\/\d+\.html'),
              callback='parse_item', follow=True),
@@ -18,6 +18,7 @@ class ChinajungongSpider(CrawlSpider):
     def parse_item(self, response):
         print(response)
         item = NewsItem()
+        print('this is a  test page hahahahah ')
         item
         item = {}
         #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
